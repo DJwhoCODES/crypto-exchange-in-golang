bff77a8dd1ab6eb7633fd8d5803917369494f8e1
DJwhoCODES <jaindevanshu824@gmail.com>
2025-10-06 03:25:11 +0530

    built orderbook, add the order to orderbook

--- orderbook.go
+++ orderbook.go
@@ -1,6 +1,17 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+type Match struct {
+	Ask        *Order
+	Bid        *Order
+	SizeFilled float64
+	Price      float64
+}
 
 type Order struct {
 	Size      float64
@@ -18,6 +29,9 @@ type Limit struct {
 type Orderbook struct {
 	Asks []*Limit
 	Bids []*Limit
+
+	AsksLimits map[float64]*Limit
+	BidsLimits map[float64]*Limit
 }
 
 func NewLimit(price float64) *Limit {
@@ -29,6 +43,7 @@ func NewLimit(price float64) *Limit {
 
 func NewOrder(bid bool, size float64) *Order {
 	return &Order{
+		Bid:       bid,
 		Size:      size,
 		Timestamp: time.Now().Unix(),
 	}
@@ -38,4 +53,90 @@ func (l *Limit) AddOrderToLimit(o *Order) {
 	o.Limit = l
 	l.Orders = append(l.Orders, o)
 	l.TotalVolume += o.Size
+}
+
+func (l *Limit) DeleteOrderFromLimit(o *Order) {
+	for i := 0; i < len(l.Orders); i++ {
+		if l.Orders[i] == o {
+			l.Orders = append(l.Orders[:i], l.Orders[i+1:]...)
+			break
+		}
+	}
+	o.Limit = nil
+	l.TotalVolume -= o.Size
+
+	sort.Slice(l.Orders, func(i, j int) bool {
+		return l.Orders[i].Timestamp < l.Orders[j].Timestamp
+	})
+}
+
+func (o *Order) String() string {
+	return fmt.Sprintf("[size: %.2f]", o.Size)
+}
+
+// func (l *Limit) String() string {
+// 	orderStr := ""
+// 	for i, o := range l.Orders {
+// 		if i > 0 {
+// 			orderStr += ", "
+// 		}
+// 		orderStr += o.String()
+// 	}
+// 	return fmt.Sprintf("[price: %.2f | orders: [%s] | totalVolume: %.2f]", l.Price, orderStr, l.TotalVolume)
+// }
+
+func NewOrderbook() *Orderbook {
+	return &Orderbook{
+		Asks:       []*Limit{},
+		Bids:       []*Limit{},
+		AsksLimits: make(map[float64]*Limit),
+		BidsLimits: make(map[float64]*Limit),
+	}
+}
+
+func (ob *Orderbook) PlaceOrder(price float64, o *Order) []Match {
+	// 1. try to match the orders using matching logic
+	// 2. add the rest of the order to the book
+	if o.Size > 0.0 {
+		ob.add(price, o)
+	}
+
+	return []Match{}
+}
+
+func (ob *Orderbook) add(price float64, o *Order) {
+	var limit *Limit
+
+	if o.Bid {
+		limit = ob.BidsLimits[price]
+	} else {
+		limit = ob.AsksLimits[price]
+	}
+
+	if limit == nil {
+		limit = NewLimit(price)
+		if o.Bid {
+			ob.Bids = append(ob.Bids, limit)
+			ob.BidsLimits[price] = limit
+		} else {
+			ob.Asks = append(ob.Asks, limit)
+			ob.AsksLimits[price] = limit
+		}
+	}
+
+	if o.Bid {
+		sort.Slice(ob.Bids, func(i, j int) bool {
+			return ob.Bids[i].Price > ob.Bids[j].Price
+		})
+	} else {
+		sort.Slice(ob.Asks, func(i, j int) bool {
+			return ob.Asks[i].Price < ob.Asks[j].Price
+		})
+	}
+
+	limit.AddOrderToLimit(o)
+
+	sort.Slice(limit.Orders, func(i, j int) bool {
+		return limit.Orders[i].Timestamp < limit.Orders[j].Timestamp
+	})
 }
--- orderbook_test.go
+++ orderbook_test.go
@@ -8,14 +8,25 @@ import (
 func TestLimit(t *testing.T) {
 	l := NewLimit(10_000)
 	buyOrderA := NewOrder(true, 5)
-	buyOrderB := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 10)
+	buyOrderC := NewOrder(true, 15)
 
 	l.AddOrderToLimit(buyOrderA)
 	l.AddOrderToLimit(buyOrderB)
+	l.AddOrderToLimit(buyOrderC)
+
+	l.DeleteOrderFromLimit(buyOrderB)
 
 	fmt.Println(l)
 }
 
 func TestOrderbook(t *testing.T) {
+	ob := NewOrderbook()
 
+	buyOrderA := NewOrder(true, 10)
+	buyOrderB := NewOrder(true, 2000)
+	ob.PlaceOrder(18_000, buyOrderA)
+	ob.PlaceOrder(19_000, buyOrderB)
+
+	fmt.Printf("%+v", ob)
 }
